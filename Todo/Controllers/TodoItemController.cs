812675063e95d134ad50859ab644ebf84c84513a
Dmitriy <dmitriy16doronin@gmail.com>
2019-12-05 11:30:39 +0700

    added mthods edit, create, update to TodoItemsManager. added method edit to TodoItemController

---
+++ Todo/Controllers/TodoItemController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Models;
+using Todo.Services;
+
+namespace Todo.Controllers
+{
+    public class TodoItemController:Controller
+    {
+        private ITodoItemManager _todoItemManager;
+        public TodoItemController(ITodoItemManager todoItemManager)
+        {
+            _todoItemManager = todoItemManager;
+        }
+        public IActionResult Edit(TodoItem todo)
+        {
+            if(ModelState.IsValid)
+            {
+                var todoItem = _todoItemManager.Update(todo);
+                if (todoItem != null)
+                {
+                    return Redirect("/Main/Index");
+                }
+            }
+            return View(todo);
+        }
+        public IActionResult AddTodoItem(TodoItem todo)
+        {
+            return View(todo);
+        }
+    }
+}
